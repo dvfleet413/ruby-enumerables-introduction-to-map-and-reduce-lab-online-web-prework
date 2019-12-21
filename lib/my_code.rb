fe0a010dae04a9643accca1a5fa75ef7c24ae552
David Van Fleet <dvfleet413@gmail.com>
2019-12-21 04:15:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -62,5 +62,13 @@ def reduce_to_all_true(array)
 end
 
 def reduce_to_any_true(array)
-  
+  i = 0 
+  while i < array.length do
+    if array[i]
+      return true
+    else
+      i += 1 
+    end
+  end
+  return false
 end
